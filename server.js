68ec0cdc7a147ec11d1717faebe252a5bb48d778
Tyanspops <122063359+Tyanspops@users.noreply.github.com>
2023-11-30 12:23:31 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express"); 
-// instantiates Express
+
 const app = express();
-// for serving files in the public folder, such as CSS files
+
 app.use(express.static("./public"));
-// server responds when the client sends a GET request for the / endpoint
+
 
 app.get("/", function(req,res) {
     res.sendFile(__dirname + "/index.html");
